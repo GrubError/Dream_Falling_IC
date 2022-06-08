a857adc6d2dc2c88fe8cbd2fa387d14335a00f7f
GrubError <fabientsiry@gamil.com>
2022-06-08 15:02:44 +0300

    Create test.cs
    ert

---
+++ test.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Input.GetKey("up");
+        Debug.Log("hallo");
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Debug.Log("hallo");
+
+            //transform.Translate(new Vector2(0f, 1f));
+
+            transform.Translate(new Vector3(0f, 1f));
+
+        }
+    }
+}
